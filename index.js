3f2f8f81c2a557e7bb6ded526554a2da72564204
kazirauf <raufelahi9876@gmail.com>
2023-11-29 23:28:21 +0600

    vercel deploy is done

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-var jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const stripe = require("stripe")(process.env.CLIENT_SECRET_KEY)
+var jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.CLIENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
-
-
-
-
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7u0ly7l.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -41,212 +35,359 @@ async function run() {
 }
 run().catch(console.dir);
 const userCollection = client.db("SurveySync").collection("users");
-const surveyCreationCollection = client.db("SurveySync").collection("surveyCreation");
+const surveyCreationCollection = client
+  .db("SurveySync")
+  .collection("surveyCreation");
 // verify token
 
 const verifyToken = (req, res, next) => {
-    console.log('inside verify token', req.headers.authorization);
-    if (!req.headers.authorization) {
-      return res.status(401).send({ message: 'unauthorized access' });
-    }
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({ message: 'unauthorized access' })
-      }
-      req.decoded = decoded;
-      next();
-    })
+  console.log("inside verify token", req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
-
-  const verifyAdmin = async (req, res, next) => {
-    const email = req.decoded.email;
-    const query = { email: email };
-    const user = await userCollection.findOne(query);
-    const isAdmin = user?.role === 'admin';
-    if (!isAdmin) {
-      return res.status(403).send({ message: 'forbidden access' });
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
     }
+    req.decoded = decoded;
     next();
+  });
+};
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === "admin";
+  if (!isAdmin) {
+    return res.status(403).send({ message: "forbidden access" });
   }
+  next();
+};
 // jwt
 
-
-
-app.post('/jwt', async (req, res) => {
+app.post("/jwt", async (req, res) => {
   const user = req.body;
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "1h" });
   res.send({ token });
-})
+});
 
-
-app.post('/surveyCreation', verifyToken, async(req, res) => {
+app.post("/surveyCreation", verifyToken, async (req, res) => {
   const item = req.body;
-  const result = await surveyCreationCollection.insertOne(item)
-  res.send(result)
-})
-
-app.get('/surveyCreation',  verifyToken,  async(req, res) => {
-  const result = await surveyCreationCollection.find().toArray()
-  res.send(result)
-})
-
-//  app.patch('/surveyCreation/Unpublish/:id', verifyToken, verifyAdmin, async (req, res) => {
-//     const id = req.params.id;
-//     const filter = { _id: new ObjectId(id) };
-//     const updatedDoc = {
-//       $set: {
-//         activity: 'unpublished'
-//       }
-//     }
-//     const result = await surveyCreationCollection.updateOne(filter, updatedDoc);
-//     res.send(result);
-//   })
-  app.get('/surveyCreation/:id', async(req, res) => {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id)};
-    const result = await surveyCreationCollection.findOne(query);
-    res.send(result);
-  })
-  app.put('/surveyCreation/Unpublish/:id', verifyToken, verifyAdmin, async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) }
-    const option = { upsert: true }
-    const updatedProducts = req.body;
-    const products = {
-        $set: {
-          activity: updatedProducts.activity,
-          feedback: updatedProducts.feedback,
-            
   
-        }
-    }
-    const result = await surveyCreationCollection.updateOne(filter, products, option);
-    res.send(result)
-  })
- app.patch('/surveyCreation/publish/:id', verifyToken, verifyAdmin, async (req, res) => {
+  item.timeStamp = new Date();
+  
+  const result = await surveyCreationCollection.insertOne(item);
+  res.send(result);
+});
+// add this Mongoose and get the survey-------
+app.get("/surveyCreation", async (req, res) => {
+  const result = await surveyCreationCollection.find().toArray();
+  res.send(result);
+});
+app.put("/surveyCreation/no/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const option = { upsert: true };
+  const updatedSurvey = req.body;
+  const survey = {
+    $set: {
+      no: updatedSurvey.no,
+    },
+  };
+  const result = await surveyCreationCollection.updateOne(
+    filter,
+    survey,
+    option
+  );
+  res.send(result);
+});
+app.put("/surveyCreation/like/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const option = { upsert: true };
+  const updatedSurvey = req.body;
+  const survey = {
+    $set: {
+      like: updatedSurvey.like,
+    },
+  };
+  const result = await surveyCreationCollection.updateOne(
+    filter,
+    survey,
+    option
+  );
+  res.send(result);
+});
+app.put("/surveyCreation/disLike/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const option = { upsert: true };
+  const updatedSurvey = req.body;
+  const survey = {
+    $set: {
+      disLike: updatedSurvey.disLike,
+    },
+  };
+  const result = await surveyCreationCollection.updateOne(
+    filter,
+    survey,
+    option
+  );
+  res.send(result);
+});
+app.post("/surveyCreation/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const option = { upsert: true };
+  const updatedSurvey = req.body;
+  console.log("Received data:", updatedSurvey);
+
+  const survey = {
+    $push: {
+      userInfo: updatedSurvey.userComment,
+    },
+  };
+
+  const result = await surveyCreationCollection.updateOne(
+    filter,
+    survey,
+    option
+  );
+  res.send(result);
+});
+
+
+
+// update survey
+
+app.put('/surveyCreation/:id', verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) }
+  const option = { upsert: true }
+  const updatedProducts = req.body;
+  const products = {
+      $set: {
+          survey_title: updatedProducts.survey_title,
+          survey_description: updatedProducts.survey_description,
+          survey_category: updatedProducts.survey_category,
+        
+       
+
+      }
+  }
+  const result = await surveyCreationCollection.updateOne(filter, products, option);
+  res.send(result)
+})
+
+
+app.post("/surveyCreation/report/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const option = { upsert: true };
+  const updatedSurvey = req.body;
+  console.log("Received data:", updatedSurvey);
+
+  const survey = {
+    $push: {
+      allUserReport: updatedSurvey.userReport,
+    },
+  };
+
+  const result = await surveyCreationCollection.updateOne(
+    filter,
+    survey,
+    option
+  );
+  res.send(result);
+});
+
+app.put("/surveyCreation/yes/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const option = { upsert: true };
+  const updatedSurvey = req.body;
+  const survey = {
+    $set: {
+      yes: updatedSurvey.yes,
+    },
+  };
+  const result = await surveyCreationCollection.updateOne(
+    filter,
+    survey,
+    option
+  );
+  res.send(result);
+});
+
+app.get("/surveyCreation/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await surveyCreationCollection.findOne(query);
+  res.send(result);
+});
+app.put(
+  "/surveyCreation/Unpublish/:id",
+  verifyToken,
+  verifyAdmin,
+  async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const option = { upsert: true };
+    const updatedSurvey = req.body;
+    const survey = {
+      $set: {
+        activity: updatedSurvey.activity,
+        feedback: updatedSurvey.feedback,
+      },
+    };
+    const result = await surveyCreationCollection.updateOne(
+      filter,
+      survey,
+      option
+    );
+    res.send(result);
+  }
+);
+
+app.patch(
+  "/surveyCreation/publish/:id",
+  verifyToken,
+  verifyAdmin,
+  async (req, res) => {
     const id = req.params.id;
     const filter = { _id: new ObjectId(id) };
     const updatedDoc = {
       $set: {
-        activity: 'published'
-      }
-    }
+        activity: "published",
+      },
+    };
     const result = await surveyCreationCollection.updateOne(filter, updatedDoc);
     res.send(result);
-  })
+  }
+);
 
-app.get('/users/admin/:email', verifyToken, async (req, res) => {
+app.get("/users/admin/:email", verifyToken, async (req, res) => {
   const email = req.params.email;
 
   if (email !== req.decoded.email) {
-    return res.status(403).send({ message: 'forbidden access' })
+    return res.status(403).send({ message: "forbidden access" });
   }
 
   const query = { email: email };
   const user = await userCollection.findOne(query);
   let admin = false;
   if (user) {
-    admin = user?.role === 'admin';
+    admin = user?.role === "admin";
   }
   res.send({ admin });
-})
+});
 
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email };
+  const existingUser = await userCollection.findOne(query);
+  if (existingUser) {
+    return res.send({ message: "user already exists", insertedId: null });
+  }
+  const result = await userCollection.insertOne(user);
+  res.send(result);
+});
 
-app.post('/users', async(req, res) => {
-   const user = req.body;
-    const query = { email: user.email }
-    const existingUser = await userCollection.findOne(query);
-    if (existingUser) {
-        return res.send({ message: 'user already exists', insertedId: null })
-     }
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-  
- })
+app.patch("/users/admin/:id", verifyToken, verifyAdmin, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedDoc = {
+    $set: {
+      role: "admin",
+    },
+  };
+  const result = await userCollection.updateOne(filter, updatedDoc);
+  res.send(result);
+});
+app.patch("/users/surveyor/:id", verifyToken, verifyAdmin, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedDoc = {
+    $set: {
+      role: "surveyor",
+    },
+  };
+  const result = await userCollection.updateOne(filter, updatedDoc);
+  res.send(result);
+});
+app.patch("/users/proUser/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedDoc = {
+    $set: {
+      role: "pro user",
+      payment: 10,
+    },
+  };
+  const result = await userCollection.updateOne(filter, updatedDoc);
+  res.send(result);
+});
 
- app.patch('/users/admin/:id',  verifyToken, verifyAdmin, async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) };
-    const updatedDoc = {
-      $set: {
-        role: 'admin'
-      }
-    }
-    const result = await userCollection.updateOne(filter, updatedDoc);
-    res.send(result);
-  })
- app.patch('/users/surveyor/:id',  verifyToken, verifyAdmin, async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) };
-    const updatedDoc = {
-      $set: {
-        role: 'surveyor'
-      }
-    }
-    const result = await userCollection.updateOne(filter, updatedDoc);
-    res.send(result);
-  }) 
- app.patch('/users/proUser/:id', verifyToken, async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) };
-    const updatedDoc = {
-      $set: {
-        role: 'pro user',
-        payment: 10
-      }
-    }
-    const result = await userCollection.updateOne(filter, updatedDoc);
-    res.send(result);
-  })
+app.delete("/users/:id", verifyAdmin, verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await userCollection.deleteOne(query);
+  res.send(result);
+});
 
-  app.delete('/users/:id', verifyAdmin, verifyToken, async (req, res) => {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) }
-    const result = await userCollection.deleteOne(query);
-    res.send(result);
-  })
-
- app.get('/users',  verifyToken, verifyAdmin, async(req, res) => {
-    const result = await userCollection.find().toArray()
-    res.send(result)
- })
- app.get('/paring', verifyToken, async(req, res) => {
-    const result = await userCollection.find().toArray()
-    res.send(result)
- })
-
-
-//  payment
-app.post('/create-payment-intent', verifyToken, async (req, res) => {
-  const { price } = req.body;
-  const amount = parseInt(price * 100);
-  console.log(amount, 'amount inside the intent')
-
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount,
-    currency: 'usd',
-    payment_method_types: ['card']
-  });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret
-  })
+app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+  const result = await userCollection.find().toArray();
+  res.send(result);
 });
 
 
+app.get("/myRole", verifyToken, async (req, res) => {
+  let query = {};
+  if (req.query?.email) {
+    query = { email: req.query.email };
+  }
+  const result = await userCollection.findOne(query);
+  res.send(result);
+});
+// for my survey page
+app.get('/mySurvey', verifyToken, async (req, res) => {
+  let query = {};
+  if (req.query?.user_email) {
+    query = { user_email: req.query.user_email };
+  }
+  
+  // Use find instead of findOne to get an array of documents
+  const result = await surveyCreationCollection.find(query).toArray();
+  
+  res.send(result);
+});
+
+
+
+//  payment
+app.post("/create-payment-intent", verifyToken, async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price * 100);
+  console.log(amount, "amount inside the intent");
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    payment_method_types: ["card"],
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// payment
+
 app.get("/", (req, res) => {
-    res.send("Welcome to our  kre-survey")
-})
+  res.send("Welcome to our  kre-survey");
+});
 app.listen(port, () => {
-    console.log(`Welcome to kre survey ${port}` );
-})
-
-
-
-
-
-
-
-
-
+  console.log(`Welcome to kre survey ${port}`);
+});
